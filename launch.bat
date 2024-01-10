ea3dc3aad7ebaedb5cc69b2403025a09dbad9b74
Knightmare1243 <regiscc07@yahoo.com>
2024-01-10 15:59:53 +0000

    added submoduals

--- launch.bat
+++ launch.bat
@@ -1,2 +1,3 @@
 cd "C:\Users\regis\My python programms\Website\Our seccond try"
-C:\Users\Regis\Downloads\hugo_0.121.2_windows-amd64\hugo.exe server
+C:\Users\Regis\Downloads\hugo_0.121.2_windows-amd64\hugo.exe
+C:\Users\Regis\Downloads\hugo_0.121.2_windows-amd64\hugo.exe server --navigateToChanged
